b7f62bde0dd2ad4c28013279f9b19971ae7ecd98
Minh <minhmac2005@gmail.com>
2026-05-28 00:33:42 +0200

    Changed normalization of u_k - still have to work on normalization of w_k

--- main.py
+++ main.py
@@ -46,7 +46,9 @@ def calculating_u_k():
             x = 0j
             for idm, m in enumerate(m_vals):
                 x += np.exp(1j*m*G*r) * c_k_m[idk][idm]
-            u_k[idk, idr] = x
+            u_k[idk, idr] = x / np.sqrt(r_vals[len(r_vals)-1] - r_vals[0])
+        area = simpson(np.abs(u_k[idk])**2, r_vals)
+
         #phi = np.angle(u_k[idk, 49])
         #u_k[idk,:] /= np.exp(1j*phi)
     return u_k
@@ -55,9 +57,12 @@ def calculating_w_k():
     u_k = calculating_u_k()
     w = np.zeros( len(r_vals), dtype= complex)
     Norm_k = len(k_vals)
-    for i, r in enumerate(r_vals):
-        for j, k in enumerate(k_vals):
-            w[i] += np.exp(1j * k  * r) * u_k[j][i] /Norm_k
+    for idr, r in enumerate(r_vals):
+        for idk, k in enumerate(k_vals):
+            w[idr] += np.exp(1j * k  * r) * u_k[idk][idr] / (np.sqrt(Norm_k))
+
+    area = simpson(np.abs(w) ** 2, r_vals)
+    print(area)
     return w
 
 def calculating_cos_pot():
@@ -66,24 +71,21 @@ def calculating_cos_pot():
         V.append(-np.cos(G*r)+np.exp(-((r-a/2)**2)))
     return V
 
-
 def disprel(k, mu, t):
     y = -2*t*np.cos(k*a) + mu
     return y
+
 parameters, covariance = curve_fit(disprel, k_vals, calculating_E_k())
 fit_mu = parameters[0]
 fit_t = parameters[1]
 fit_disprel = disprel(k_vals, fit_mu, fit_t)
-print(fit_mu)
-print(fit_t)
+#print(fit_mu)
+#print(fit_t)
 SE = np.sqrt(np.diag(covariance))
 SE_A = SE[0]
 SE_B = SE[1]
-print(F'The value of A is {fit_mu:.5f} with standard error of {SE_A:.5f}.')
-print(F'The value of B is {fit_t:.5f} with standard error of {SE_B:.5f}.')
-
-plt.plot(k_vals, calculating_E_k(), 'o', label='data')
-plt.plot(k_vals, fit_disprel, '-', label='fit')
+#print(F'The value of A is {fit_mu:.5f} with standard error of {SE_A:.5f}.')
+#print(F'The value of B is {fit_t:.5f} with standard error of {SE_B:.5f}.')
 
 w_0 = calculating_w_k()
 u_k = calculating_u_k()
@@ -96,9 +98,11 @@ y_5 = calculating_cos_pot()
 
 
 #plt.plot(r_vals, y_5, label="Potential")
-#plt.plot(r_vals, y_2, label="Wannier")
+plt.plot(r_vals, y_2, label="Wannier")
 #plt.plot(k_vals, y_4, label = "E_k")
+#plt.plot(k_vals, calculating_E_k(), 'o', label='data')
+#plt.plot(k_vals, fit_disprel, '-', label='fit')
 
 plt.legend()
-#plt.xlim(-2*a,2*a)
+plt.xlim(-2*a,2*a)
 plt.show()
