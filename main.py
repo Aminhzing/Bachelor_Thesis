-fixed normalization (u_k is only 1 for one cell site, periodicity) -> w_k normalized with / N_k why? - fix calculation of dt

main.py
@@ -3,16 +3,16 @@ from scipy.optimize import curve_fit
 from scipy.integrate import simpson
 import matplotlib.pyplot as plt
 
-U = 0.5
+U = 1
 a = 1
 G = 2*np.pi/a
-dim = 50
+dim = 100
 
 
 m_vals = np.arange(-dim, dim+1)
-r_vals = np.linspace(-2*a,2*a,100)
+r_vals = np.linspace(-2*a,2*a,400)
 k_vals = np.linspace(-np.pi/a,np.pi/a,100)
-
+r_valscell = np.linspace(-a/2,a/2, 100)
 
 def calc_c_k_m():
     main_diag = np.zeros((len(k_vals), len(m_vals)))
@@ -47,8 +47,8 @@ def calc_u_k():
             x = 0j
             for idm, m in enumerate(m_vals):
                 x += np.exp(1j*m*G*r) * c_k_m[idk][idm]
-            u_k[idk, idr] = x / np.sqrt(r_vals[len(r_vals)-1] - r_vals[0])
-        area = simpson(np.abs(u_k[idk])**2, r_vals)
+            u_k[idk, idr] = x
+            #area = simpson(np.abs(u_k[idk, idr])**2, r_valscell)
         #print(area)
     return u_k
 
@@ -58,8 +58,8 @@ def calc_w_k():
     for idr, r in enumerate(r_vals):
         for idk, k in enumerate(k_vals):
             w[idr] += np.exp(1j * k  * r) * u_k[idk][idr] / len(k_vals)
-    area = simpson(np.abs(w) ** 2, r_vals)
-    print(area)
+    #area = simpson(np.abs(w) ** 2, r_vals)
+    #print('area = ', area)
     return w
 
 def calc_pot():
@@ -105,10 +105,10 @@ def plot_w_0():
 
     plt.figure()
     plt.plot(r_vals, pertpot, label="Potential")
-    plt.plot(r_vals, np.conj(calc_w_k())*calc_w_k(), label="Wannier")
-    plt.xlim(-2*a,2*a)
+    plt.plot(r_vals, calc_w_k(), label="Wannier")
+    plt.xlim(-4*a,4*a)
     plt.legend()
-    plt.ylabel("|psi|²")
+    plt.ylabel(|psi|)
     plt.xlabel("Lattice sites")
     plt.show()
 
@@ -117,20 +117,18 @@ def calc_dmu_dt():
     dr = r_vals[1] - r_vals[0]
     w_0 = calc_w_k()
     _ , pert = calc_pot()
-    r_valscell = np.linspace(-a,a, 50)
     dmu = simpson(np.abs(w_0)**2*pert, r_vals)
-    #for idr, r in enumerate(r_vals):
-        #dmu += dr* np.abs(w_0[idr])**2 * pert[idr]
     print( 'dmu = ',dmu)
     w_1 = np.roll(w_0, len(r_vals)//4)
     for idr in range(len(r_valscell)):
-        #w_1.append(w_0[idr - len(r_vals) // 4])
-        dt += dr * np.conj(w_0[idr + len(r_valscell) // 2]) * w_1[idr + len(r_valscell) // 2] * pert[idr + len(r_valscell) // 2]
+        dt += dr * np.conj(w_0[idr + len(r_vals) // 2]) * w_1[idr + len(r_vals) // 2] * pert[idr + len(r_valscell) // 2]
     print('dt = ', dt)
     plt.plot(r_vals, pert, 'o', label='pert')
     plt.plot(r_vals, np.real(w_1), '-', label='w_1')
     plt.plot(r_vals, np.real(w_0) , '-', label='w_0')
     plt.show()
 
-fit_disp()
-calc_dmu_dt()
+#calc_w_k()
+plot_w_0()
+#fit_disp()
+#calc_dmu_dt()